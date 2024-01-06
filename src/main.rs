9ac72c041220a57e77c2cfc7ed7b81a3e752b542
HellOwhatAs <xjq701229@outlook.com>
2024-01-06 20:09:22 +0800

    `expr` without `malloc` and `free`

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,104 @@
-use nom::IResult;
+use std::collections::HashMap;
 use std::error::Error;
+use nom::IResult;
 use nom::character::complete as c;
 use nom::branch::alt;
 use nom::sequence::{tuple, delimited};
 use nom::bytes::complete::tag;
 use nom::multi::many0;
+use nom::combinator::{recognize, opt};
+use text_io::read;
 
 #[derive(Debug)]
 pub enum Expr<'a> {
     BinOp(&'a str, Box<Expr<'a>>, Box<Expr<'a>>),
-    Int(i64)
+    UnOp(&'a str, Box<Expr<'a>>),
+    Call(&'a str, Option<Box<Expr<'a>>>),
+    Ident(&'a str),
+    Int(u128)
 }
 
 impl<'a> Expr<'a> {
-    fn eval(&self) -> i64 {
+    fn eval(&self, registers: &'a HashMap<&'a str, i128>) -> i128 {
         match self {
             Expr::BinOp(op, l, r) => match op {
-                &"+" => l.eval() + r.eval(),
-                &"-" => l.eval() - r.eval(),
-                &"*" => l.eval() * r.eval(),
-                &"/" => l.eval() / r.eval(),
-                &"^" => l.eval().pow(r.eval().try_into().unwrap()),
+                &"+" => l.eval(registers) + r.eval(registers),
+                &"-" => l.eval(registers) - r.eval(registers),
+                &"*" => l.eval(registers) * r.eval(registers),
+                &"/" => l.eval(registers) / r.eval(registers),
+                &"%" => l.eval(registers) % r.eval(registers),
+                &"^" => l.eval(registers).pow(r.eval(registers).try_into().unwrap()),
+                &">" => (l.eval(registers) > r.eval(registers)) as i128,
+                &">=" => (l.eval(registers) >= r.eval(registers)) as i128,
+                &"<" => (l.eval(registers) < r.eval(registers)) as i128,
+                &"<=" => (l.eval(registers) <= r.eval(registers)) as i128,
+                &"==" => (l.eval(registers) == r.eval(registers)) as i128,
+                &"!=" => (l.eval(registers) != r.eval(registers)) as i128,
+                &"&&" => (l.eval(registers) !=0  && r.eval(registers) != 0) as i128,
+                &"||" => (l.eval(registers) !=0  || r.eval(registers) != 0) as i128,
                 _ => unreachable!()
             },
-            Expr::Int(i) => *i,
+            Expr::UnOp(op, e) => match op {
+                &"+" => e.eval(registers),
+                &"-" => - e.eval(registers),
+                &"*" => todo!(),
+                &"!" => (e.eval(registers) == 0) as i128,
+                _ => unreachable!()
+            },
+            Expr::Call(fname, opt_e) => match (fname, opt_e) {
+                (&"write_int", Some(e)) => {
+                    let res = e.eval(registers);
+                    print!("{}", res);
+                    res
+                },
+                (&"write_char", Some(e)) => {
+                    let res = e.eval(registers);
+                    print!("{}", res as u8 as char);
+                    res
+                },
+                (&"read_int", None) => {
+                    let res: i128 = read!();
+                    res
+                },
+                (&"read_char", None) => {
+                    let res: char = read!();
+                    res as i128
+                },
+                _ => unreachable!()
+            },
+            Expr::Int(i) => (*i).try_into().unwrap(),
+            Expr::Ident(x) => *registers.get(x).unwrap_or(&0)
         }
     }
 }
 
-pub fn parse_i64(input: &str) -> IResult<&str, Box<Expr>> {
-    let (rem, res) = c::i64(input)?;
+pub fn identifier<'a>(s: &'a str) -> IResult<&'a str, &'a str> {
+    recognize(tuple((
+        alt((tag("_"), c::alpha1)),
+        many0(alt((tag("_"), c::alphanumeric1)))
+    )))(s)
+}
+
+pub fn parse_u128(input: &str) -> IResult<&str, Box<Expr>> {
+    let (rem, res) = c::u128(input)?;
     Ok((rem, Box::new(Expr::Int(res))))
+}
+
+pub fn parse_ident(input: &str) -> IResult<&str, Box<Expr>> {
+    let (rem, res) = identifier(input)?;
+    Ok((rem, Box::new(Expr::Ident(res))))
+}
+
+pub fn parse_call(input: &str) -> IResult<&str, Box<Expr>> {
+    let (rem, res) = tuple((identifier, tag("("), opt(parse_expr), tag(")")))(input)?;
+    Ok((rem, Box::new(Expr::Call(res.0, res.2))))
 }
 
 pub fn parse_single(input: &str) -> IResult<&str, Box<Expr>> {
     alt((
-        parse_i64,
+        parse_u128,
+        parse_call,
+        parse_ident,
         delimited(tag("("), parse_expr, tag(")")),
     ))(input)
 }
@@ -43,22 +106,68 @@ pub fn parse_single(input: &str) -> IResult<&str, Box<Expr>> {
 pub fn parse_pow(input: &str) -> IResult<&str, Box<Expr>> {
     match tuple((parse_single, tag("^"), parse_pow))(input) {
         Ok((rem, res)) => Ok((rem, Box::new(Expr::BinOp("^", res.0, res.2)))),
-        _ => {
-            parse_single(input)
-        },
+        _ => parse_single(input),
     }
 }
 
+pub fn parse_higher_unop(input: &str) -> IResult<&str, Box<Expr>> {
+    fn higher_unop(input: &str) -> IResult<&str, Box<Expr>> {
+        let (rem, res) = tuple((tag("*"), parse_higher_unop))(input)?;
+        Ok((rem, Box::new(Expr::UnOp(res.0, res.1))))
+    }
+    alt((higher_unop, parse_pow))(input)
+}
+
 pub fn parse_higher_binop(input: &str) -> IResult<&str, Box<Expr>> {
-    let (rem, (mut res, res1)) = tuple((parse_pow, many0(tuple((alt((tag("*"), tag("/"))), parse_pow)))))(input)?;
+    let (rem, (mut res, res1)) = tuple((parse_higher_unop, many0(tuple((alt((tag("*"), tag("/"), tag("%"))), parse_higher_unop)))))(input)?;
     for (op, e) in res1.into_iter() {
         res = Box::new(Expr::BinOp(op, res, e));
     }
     Ok((rem, res))
 }
 
+pub fn parse_lower_unop(input: &str) -> IResult<&str, Box<Expr>> {
+    fn lower_unop(input: &str) -> IResult<&str, Box<Expr>> {
+        let (rem, res) = tuple((alt((tag("+"), tag("-"))), parse_lower_unop))(input)?;
+        Ok((rem, Box::new(Expr::UnOp(res.0, res.1))))
+    }
+    alt((lower_unop, parse_higher_binop))(input)
+}
+
 pub fn parse_lower_binop(input: &str) -> IResult<&str, Box<Expr>> {
-    let (rem, (mut res, res1)) = tuple((parse_higher_binop, many0(tuple((alt((tag("+"), tag("-"))), parse_higher_binop)))))(input)?;
+    let (rem, (mut res, res1)) = tuple((parse_lower_unop, many0(tuple((alt((tag("+"), tag("-"))), parse_lower_unop)))))(input)?;
+    for (op, e) in res1.into_iter() {
+        res = Box::new(Expr::BinOp(op, res, e));
+    }
+    Ok((rem, res))
+}
+
+pub fn parse_cmp_binop(input: &str) -> IResult<&str, Box<Expr>> {
+    let (rem, (mut res, res1)) = tuple((parse_lower_binop, many0(tuple((alt((tag(">"), tag(">="), tag("<"), tag("<="), tag("=="), tag("!="))), parse_lower_binop)))))(input)?;
+    for (op, e) in res1.into_iter() {
+        res = Box::new(Expr::BinOp(op, res, e));
+    }
+    Ok((rem, res))
+}
+
+pub fn parse_not_unop(input: &str) -> IResult<&str, Box<Expr>> {
+    fn not_unop(input: &str) -> IResult<&str, Box<Expr>> {
+        let (rem, res) = tuple((tag("!"), parse_not_unop))(input)?;
+        Ok((rem, Box::new(Expr::UnOp(res.0, res.1))))
+    }
+    alt((not_unop, parse_cmp_binop))(input)
+}
+
+pub fn parse_and_binop(input: &str) -> IResult<&str, Box<Expr>> {
+    let (rem, (mut res, res1)) = tuple((parse_not_unop, many0(tuple((tag("&&"), parse_not_unop)))))(input)?;
+    for (op, e) in res1.into_iter() {
+        res = Box::new(Expr::BinOp(op, res, e));
+    }
+    Ok((rem, res))
+}
+
+pub fn parse_or_binop(input: &str) -> IResult<&str, Box<Expr>> {
+    let (rem, (mut res, res1)) = tuple((parse_and_binop, many0(tuple((tag("||"), parse_and_binop)))))(input)?;
     for (op, e) in res1.into_iter() {
         res = Box::new(Expr::BinOp(op, res, e));
     }
@@ -66,12 +175,14 @@ pub fn parse_lower_binop(input: &str) -> IResult<&str, Box<Expr>> {
 }
 
 pub fn parse_expr(input: &str) -> IResult<&str, Box<Expr>> {
-    parse_lower_binop(input)
+    parse_or_binop(input)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let (remaining_input, output) = parse_expr("1000000/(1+2*3^4+5-7*5474/9110)")?;
+    let (remaining_input, output) = parse_expr("write_int(read_int()*k)+write_char(10)-10")?;
+    let mut registers = HashMap::new();
+    registers.insert("k", 3000);
     println!("{:?} {:?}", remaining_input, output);
-    println!("{}", output.eval());
-  Ok(())
+    println!("{}", output.eval(&registers));
+    Ok(())
 }
